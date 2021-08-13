d82d68e46bee04d13079345e0a9716cedd60097d
Ran <51711790+ransananes@users.noreply.github.com>
2021-08-13 13:13:33 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,6 +10,16 @@ const socket = require("socket.io");
 const io = socket(server, {cors: true});
 app.use(cors());
 
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 // currenty global room
 const room = {users:[],gameStarted: false,words_used: [],syllable: "",roundTime: 0,currentUser: 0,messages: [],}
 var fs = require('fs')
