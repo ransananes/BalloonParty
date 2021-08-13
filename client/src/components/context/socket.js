f968e44402a902193b02b7976c4d2b1bae413ee8
ransananes <51711790+ransananes@users.noreply.github.com>
2021-08-13 18:57:22 +0300

    fix

--- client/src/components/context/socket.js
+++ client/src/components/context/socket.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 import React from 'react';
 
-//export const SOCKET_URL = "https://balloonparty.herokuapp.com/";
-export const SOCKET_URL = "http://localhost:5000/";
+export const SOCKET_URL = "https://balloonparty.herokuapp.com/";
+//export const SOCKET_URL = "http://localhost:5000/";
 export const socket = io(SOCKET_URL, { secure: true });
 export const SocketContext = React.createContext(socket);
