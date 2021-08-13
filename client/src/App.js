5710d552fe5d23bcb4bb5668dc2a2c0252d66ab3
ransananes <51711790+ransananes@users.noreply.github.com>
2021-08-13 15:18:08 +0300

    fix?

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,12 @@ import Lobby from './components/pages/Lobby';
 import Room from './components/pages/Room';
 function App() {
   const history = createBrowserHistory({forceRefresh:true});
-  App.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
-  });
   return (
     <div>
         <Router history={history}>
         <Switch>
         <Route exact path='/' component={Lobby}/>
-        <Route path='/Room' component={Room} />
+        <Route exact path='/Room' component={Room} />
       </Switch>
       </Router>
     </div>
