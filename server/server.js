9eb944c234896510e89eee56a941e8fb2959d3ba
ransananes <51711790+ransananes@users.noreply.github.com>
2021-08-13 16:51:42 +0300

    fix

--- server/server.js
+++ server/server.js
@@ -14,9 +14,10 @@ app.use(cors());
   // Exprees will serve up production assets
   app.use(express.static('client/build'));
 
-  app.get('/*', function(req, res) {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-})
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname + '/client/build/index.html'))
+  })
+
 
 
 // currenty global room
